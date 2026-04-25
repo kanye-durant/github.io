e6366b6682f74340de4c5d16aa7fc537fb38c91d
kanye-durant <wutongtangdzj@qq.com>
2026-04-25 19:43:50 +0800

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,12 @@ export default defineConfig({
         help: resolve(__dirname, 'help.html'),
         tougao: resolve(__dirname, 'tougao.html'),
         sanwen: resolve(__dirname, 'sanwen.html'),
-        category: resolve(__dirname, 'category.html')
+        category: resolve(__dirname, 'category.html'),
+        article: resolve(__dirname, 'article.html'),
+        author: resolve(__dirname, 'author.html'),
+        search: resolve(__dirname, 'search.html'),
+        column: resolve(__dirname, 'column.html'),
+        'column-more': resolve(__dirname, 'column-more.html')
       }
     }
   }
